6afb4519aada86c9e90741fbd9a1e29892d9f8c7
daezenriquez-pixel <daez.enriquez@gmail.com>
2026-08-12 19:36:57 -0500

    Delete backend directory

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, createAdmin, registerUser } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/register', registerUser);
-router.post('/admin/create', createAdmin); // Endpoint exclusivo para el superusuario
-
-module.exports = router;
